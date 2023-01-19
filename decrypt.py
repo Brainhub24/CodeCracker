aeb087421b35c1f9f73ecf27816139896b447a1f
Jason Prime <600613.m6@gmail.com>
2023-01-19 12:04:47 +0100

    Upgrades
    Added the argparse module to add flag options and help descriptions to the program.
    Additionally I added the encryption from plain text to base64 string.
    
    Some flags can be used now:
    -e & --encode for encode a plain string
    -d & --decode for decode a base64 string
    -f --file for decode base64 string from a file

--- decrypt.py
+++ decrypt.py
@@ -2,8 +2,27 @@
 # Simple way just on Linux commandline:
 # echo "R2l0aHViQEJyYWluaHViMjQgfCBiYXNlMzIKZWNobyBHaXRodWJAQnJhaW5odWIyNAo=" | base64 -d
 # Result: Github@Brainhub24
+import argparse
 import base64
 
-encoded_string = "R2l0aHViQEJyYWluaHViMjQgfCBiYXNlMzIKZWNobyBHaXRodWJAQnJhaW5odWIyNAo="
-decoded_string = base64.b64decode(encoded_string)
-print(decoded_string)
+def decode(encoded_string):
+    decoded_string = base64.b64decode(encoded_string)
+    print(decoded_string)
+
+parser = argparse.ArgumentParser(description='Decode base64 encoded strings')
+parser.add_argument('-d', '--decode', help='decode the base64 string')
+parser.add_argument('-e', '--encode', help='encode the plain string')
+parser.add_argument('-f', '--file', help='decode the base64 string from file')
+args = parser.parse_args()
+
+if args.decode:
+    decode(args.decode)
+elif args.file:
+    with open(args.file, 'r') as f:
+        encoded_string = f.read()
+        decode(encoded_string)
+elif args.encode:
+    encoded_string = base64.b64encode(args.encode.encode())
+    print(encoded_string)
+else:
+    parser.print_help()
